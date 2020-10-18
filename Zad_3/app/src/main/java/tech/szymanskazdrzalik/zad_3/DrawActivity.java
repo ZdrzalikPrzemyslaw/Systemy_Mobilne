4c1c2d439fde857eb3641d48bf6dad7865d9bade
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-10-18 15:50:06 +0200

    added blut

--- Zad_3/app/src/main/java/tech/szymanskazdrzalik/zad_3/DrawActivity.java
+++ Zad_3/app/src/main/java/tech/szymanskazdrzalik/zad_3/DrawActivity.java
@@ -3,6 +3,7 @@ package tech.szymanskazdrzalik.zad_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class DrawActivity extends AppCompatActivity {
     private DrawView drawView;
@@ -10,8 +11,20 @@ public class DrawActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        drawView = new DrawView(this);
-        setContentView(drawView);
-        drawView.requestFocus();
+        setContentView(R.layout.activity_draw);
+        drawView = (DrawView) findViewById(R.id.drawView);
     }
+
+    public void eraseButtonOnClick(View v) {
+        this.drawView.erase();
+    }
+
+    public void setNormalButtonOnClick(View v) {
+        this.drawView.setNormal();
+    }
+
+    public void setBlurButtonOnClick(View v) {
+        this.drawView.setBlur();
+    }
+
 }
--- Zad_3/app/src/main/java/tech/szymanskazdrzalik/zad_3/DrawView.java
+++ Zad_3/app/src/main/java/tech/szymanskazdrzalik/zad_3/DrawView.java
@@ -1,30 +1,83 @@
 package tech.szymanskazdrzalik.zad_3;
 
 import android.content.Context;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+
+import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class DrawView extends View implements View.OnTouchListener{
+public class DrawView extends View implements View.OnTouchListener {
 
-    private Paint paint = new Paint();
-    private List<Point> pointList = new ArrayList<>();
+    private final Paint paintNormal = new Paint();
+    private final Paint paintBlured = new Paint();
+    private final List<Paint> paints = new ArrayList<>();
+
+    private final List<Point> pointListNormal = new ArrayList<>();
+    private final List<Point> pointListBlur = new ArrayList<>();
+    private List<Point> chosenList = pointListNormal;
+    private final List<List<Point>> allPointLists = new ArrayList<>();
 
     /**
      * {@inheritDoc}
      */
     public DrawView(Context context) {
         super(context);
+        initView();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public DrawView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public DrawView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public DrawView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initView();
+    }
+
+    private void initView() {
         setFocusable(true);
         setFocusableInTouchMode(true);
         this.setOnTouchListener(this);
-        paint.setColor(Color.RED);
+        this.initPaints();
+        this.initPointLists();
+    }
+
+    private void initPaints() {
+        paints.add(this.paintNormal);
+        paints.add(this.paintBlured);
+        for (Paint i : paints) {
+            i.setColor(Color.RED);
+        }
+        paintBlured.setMaskFilter(new BlurMaskFilter(8, BlurMaskFilter.Blur.NORMAL));
+    }
+
+    private void initPointLists() {
+        this.allPointLists.add(pointListBlur);
+        this.allPointLists.add(pointListNormal);
     }
 
     /**
@@ -35,15 +88,34 @@ public class DrawView extends View implements View.OnTouchListener{
         Point point = new Point();
         point.x = (int) event.getX();
         point.y = (int) event.getY();
-        pointList.add(point);
+        chosenList.add(point);
         invalidate();
         return true;
     }
 
+    public void erase() {
+        for (List<Point> i : this.allPointLists) {
+            i.clear();
+        }
+        this.invalidate();
+    }
+
+    public void setBlur() {
+        this.chosenList = this.pointListBlur;
+    }
+
+    public void setNormal() {
+        this.chosenList = this.pointListNormal;
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
-        for(Point point: pointList) {
-            canvas.drawCircle(point.x, point.y, 30, paint);
+        for (Point point : pointListNormal) {
+            canvas.drawCircle(point.x, point.y, 30, paintNormal);
+        }
+
+        for (Point point : pointListBlur) {
+            canvas.drawCircle(point.x, point.y, 30, paintBlured);
         }
     }
 }
