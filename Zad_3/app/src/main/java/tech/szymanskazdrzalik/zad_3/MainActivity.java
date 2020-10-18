b48eabc169d69cb5263af1f2f6d9b122a10283f8
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-10-18 13:01:07 +0200

    remove actin  bar

---
+++ Zad_3/app/src/main/java/tech/szymanskazdrzalik/zad_3/MainActivity.java
@@ -0,0 +1,16 @@
+package tech.szymanskazdrzalik.zad_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
