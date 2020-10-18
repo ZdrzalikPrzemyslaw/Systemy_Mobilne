0a159ee25935f64b85a3c192ba509a287d47de41
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-10-18 18:15:19 +0200

    colour picker done

--- Zad_3/app/src/main/java/tech/szymanskazdrzalik/zad_3/DrawActivity.java
+++ Zad_3/app/src/main/java/tech/szymanskazdrzalik/zad_3/DrawActivity.java
@@ -2,8 +2,11 @@ package tech.szymanskazdrzalik.zad_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+
+import top.defaults.colorpicker.ColorPickerPopup;
 
 public class DrawActivity extends AppCompatActivity {
     private DrawView drawView;
@@ -33,6 +36,24 @@ public class DrawActivity extends AppCompatActivity {
 
     public void eraserButtonOnClick(View v) {
         this.drawView.eraser();
+    }
+
+    public void colourButtonOnCLick(View v) {
+        new ColorPickerPopup.Builder(this)
+                .initialColor(Color.RED) // Set initial color
+                .enableBrightness(true) // Enable brightness slider or not
+                .enableAlpha(true) // Enable alpha slider or not
+                .okTitle("Choose")
+                .cancelTitle("Cancel")
+                .showIndicator(true)
+                .showValue(true)
+                .build()
+                .show(v, new ColorPickerPopup.ColorPickerObserver() {
+                    @Override
+                    public void onColorPicked(int color) {
+                        drawView.setChosenColour(color);
+                    }
+                });
     }
 
 }
